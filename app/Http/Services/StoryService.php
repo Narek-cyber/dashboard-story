40ed186219a80d831d130266b3ee42f3a3b2787a
Narek <narsahakyan.work@gmail.com>
2024-07-01 17:07:34 +0400

    add AdminSeeder

--- app/Http/Services/StoryService.php
+++ app/Http/Services/StoryService.php
@@ -3,7 +3,6 @@
 namespace App\Http\Services;
 
 use App\Jobs\SendNotificationJob;
-use App\Jobs\SendNotifiicationJob;
 use App\Models\Story;
 use Illuminate\Database\Eloquent\Collection;
 
---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $admins = [
+            [
+                'name' => 'John Doe',
+                'email' => 'sahakyannarek023@gmail.com',
+                'password' => Hash::make('12345678'),
+            ],
+        ];
+
+        foreach ($admins as $admin) {
+            User::query()->firstOrCreate(['email' => $admin['email']], $admin);
+        }
+
+    }
+}
