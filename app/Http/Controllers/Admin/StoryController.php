92056acd0c64009ecb2c4861a0088c705dce2179
Narek <narsahakyan.work@gmail.com>
2024-07-08 14:11:42 +0400

    remove unused code

--- app/Http/Controllers/Admin/StoryController.php
+++ app/Http/Controllers/Admin/StoryController.php
@@ -12,7 +12,6 @@ use Illuminate\Contracts\View\View;
 use Illuminate\Foundation\Application;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
 class StoryController extends Controller
