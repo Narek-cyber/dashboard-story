f0192a3fab8d1f89bbdbf14b6ea533d8ef2a6c62
Narek <narsahakyan.work@gmail.com>
2024-07-17 11:09:06 +0400

    fix migration rollback

--- database/migrations/2024_07_16_114334_add_token_column_to_stories_table.php
+++ database/migrations/2024_07_16_114334_add_token_column_to_stories_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('stories', function (Blueprint $table) {
-            $table->dropColumn('role');
+            $table->dropColumn('approval_token');
         });
     }
 };
